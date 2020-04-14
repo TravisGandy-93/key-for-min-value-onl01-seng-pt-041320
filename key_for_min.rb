1dc95f9de3e575020d79dac011673c2c7d2a1242
Travis Gandy <travis_gandy@yahoo.com>
2020-04-14 19:37:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,5 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil 
   name_hash.each do |key, val|
-    if lowest_value = nil || val <
+    if lowest_value = nil || val < lowest_value
 end
